87dc8da01a44290383166a9cb523609df922784f
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2020-12-09 08:13:00 -0500

    Enum, Enemy Object

---
+++ Enemies.java
@@ -0,0 +1,22 @@
+package rockpapershowdown;
+
+/**
+ *
+ * @author liam
+ */
+enum Attack {
+  R,
+  P,
+  S
+}
+public class Enemies {
+    private String name;
+    private int hp, rock, paper, scissors;
+   
+   
+    
+
+public Enemy {
+    
+}
+}
