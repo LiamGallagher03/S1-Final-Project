53b977bec6e5e3a2354ee0b62291f4c5acfe3c68
Liam Gallagher <71441726+LiamGallagher03@users.noreply.github.com>
2020-12-11 22:56:12 -0500

    Revamped fight class.

---
+++ Enemy.java
@@ -0,0 +1,17 @@
+package rockpapershowdown;
+
+//liam
+
+public class Enemy {
+    private String name;
+    private int hp;
+public Enemy(String name, int hp) {
+    this.name = name;
+    this.hp = hp;
+}
+    public int getHp() {
+        return hp;
+    }
+    
+    
+}
---
+++ Player.java
@@ -0,0 +1,21 @@
+package rockpapershowdown;
+
+//liam
+
+public class Player {
+    private int hp, attack;
+            
+public Player() {
+    this.hp = 100;
+    this.attack = 0;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = (int) 100;
+    }
+
+}
